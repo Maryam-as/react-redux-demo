6923e62471875df2e5ac740565807e91724f898a
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-07-16 20:51:29 +0330

    refactor(counter): convert functional component to class-based

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,32 +1,55 @@
 import { useSelector, useDispatch } from "react-redux";
+import { Component } from "react";
 
 import classes from "./Counter.module.css";
 
-const Counter = () => {
-  const counter = useSelector((state) => state.counter);
-  const dispatch = useDispatch();
+// const Counter = () => {
+//   const counter = useSelector((state) => state.counter);
+//   const dispatch = useDispatch();
 
-  const incrementHandler = () => {
-    dispatch({ type: "increment" });
-  };
+//   const incrementHandler = () => {
+//     dispatch({ type: "increment" });
+//   };
 
-  const decrementHandler = () => {
-    dispatch({ type: "decrement" });
-  };
+//   const decrementHandler = () => {
+//     dispatch({ type: "decrement" });
+//   };
 
-  const toggleCounterHandler = () => {};
+//   const toggleCounterHandler = () => {};
 
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
-      <div>
-        <button onClick={incrementHandler}>Increment</button>
-        <button onClick={decrementHandler}>Decrement</button>
-      </div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
-  );
-};
+//   return (
+//     <main className={classes.counter}>
+//       <h1>Redux Counter</h1>
+//       <div className={classes.value}>{counter}</div>
+//       <div>
+//         <button onClick={incrementHandler}>Increment</button>
+//         <button onClick={decrementHandler}>Decrement</button>
+//       </div>
+//       <button onClick={toggleCounterHandler}>Toggle Counter</button>
+//     </main>
+//   );
+// };
+
+class Counter extends Component {
+  incrementHandler() {}
+
+  decrementHandler() {}
+
+  toggleCounterHandler() {}
+
+  render() {
+    return (
+      <main className={classes.counter}>
+        <h1>Redux Counter</h1>
+        <div className={classes.value}>{counter}</div>
+        <div>
+          <button onClick={this.incrementHandler}>Increment</button>
+          <button onClick={this.decrementHandler}>Decrement</button>
+        </div>
+        <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+      </main>
+    );
+  }
+}
 
 export default Counter;
